43b8b41e51e943b2557e58153c9a830fe6cdd595
Sylveon Shi <123628282+SylveonShi@users.noreply.github.com>
2024-11-14 22:00:26 -0600

    Add files via upload
    Comments added

--- WinnerPicker.java
+++ WinnerPicker.java
@@ -3,29 +3,57 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
 
+/**
+ * This class picks a random winner from a list of participants. To imitate random picker similar
+ * to the website, a number of times of random number generation will be conducted to pick the
+ * winner.
+ */
 public class WinnerPicker {
-    private String[] list = null;
-    private String contestors = "";
-    private int times = 100;
-    private String winner = "";
-    private boolean testMode = false;
+    private String[] list = null; // Store the names of the participants, but in list form
+    private String contestors = ""; // Store the names of the participants, but in String form
+    private int times = 100; // Times of generation of random winner
+    private String winner = ""; // The winner name
+    private boolean testMode = false; // Indicate whether it is the test mode
 
+    /**
+     * This method returns the status of program (whether it is in test or not)
+     *
+     * @return status of program
+     */
     public boolean getTestMode() {
         return this.testMode;
     }
 
+    /**
+     * This method sets the status of program (whether it is in test or not)
+     *
+     * @param testMode status of program user wishes to set.
+     */
     public void setTestMode(boolean testMode) {
         this.testMode = testMode;
     }
 
+    /**
+     * This method returns the name of the winner
+     *
+     * @return the name of the winner
+     */
     public String getWinner() {
         return this.winner;
     }
 
+    /**
+     * This method returns the list of the participants
+     *
+     * @return list of participants
+     */
     public String[] getList() {
         return this.list;
     }
 
+    /**
+     * This method read in the data in the Names.csv and get the list of the participants
+     */
     public void readData() {
         try (BufferedReader reader = new BufferedReader(new FileReader("src/Names.csv"))) {
             contestors = reader.readLine();
@@ -35,6 +63,10 @@ public class WinnerPicker {
         }
     }
 
+    /**
+     * This method conducts the generation of random numbers for many times and call the
+     * resultPrinter method to print out the result on time.
+     */
     public void randomPicker() {
         Random rand = new Random();
         for (int i = 0; i < times; i++) {
@@ -52,6 +84,11 @@ public class WinnerPicker {
         }
     }
 
+    /**
+     * This method prints out the result of each pick
+     *
+     * @param luckyIndex The random number generated (represented the winner in this generation)
+     */
     public void resultPrinter(int luckyIndex) {
         for (int i = 0; i < list.length; i++) {
             if (i != luckyIndex) {
@@ -68,15 +105,23 @@ public class WinnerPicker {
 
     }
 
-    public void runPicker(WinnerPicker picker) {
-        picker.readData();
-        picker.randomPicker();
-    }
-
+    /**
+     * This method "clear" the console by printing 50 new lines to keep the console clear when
+     * generating result of a new pick.
+     */
     public static void clearConsole() {
         for (int i = 0; i < 50; i++) {
             System.out.println();
         }
+    }
+
+    /**
+     * This method
+     * @param picker
+     */
+    public void runPicker(WinnerPicker picker) {
+        picker.readData();
+        picker.randomPicker();
     }
 
     public static void main(String[] args) {
--- WinnerPickerTester.java
+++ WinnerPickerTester.java
@@ -1,12 +1,21 @@
+/**
+ * This class is a tester class for demonstrating the fairness of the WinnerPicker class. The
+ * WinnerPicker will be called for a number of times and total times of being picked for each
+ * people will be kept and printed to see whether the random winner picker is truly random.
+ */
 public class WinnerPickerTester {
-    private static int repeatTimes = 1000;
-    private static int[] counts;
-    private static String[] candidates;
+    private static int repeatTimes = 1000; // Number of times the WinnerPicker is called
+    private static int[] counts; // Number of times each person is picked.
+    private static String[] candidates; // List of participants
 
+    /**
+     * This class starts the test
+     * @param args not used here
+     */
     public static void main(String[] args) {
         WinnerPicker test = new WinnerPicker();
         test.readData();
-        String[] candidates = test.getList();
+        candidates = test.getList();
         counts = new int[candidates.length];
         test.setTestMode(true);
         for (int i = 0; i < repeatTimes; i++) {
@@ -17,12 +26,17 @@ public class WinnerPickerTester {
                 }
             }
         }
-        graphPrinter();
+        resultPrinter();
 
     }
-    public static void graphPrinter(){
+
+    /**
+     * This class print out the result of the test
+     */
+    public static void resultPrinter() {
         for (int i = 0; i < counts.length; i++) {
-            System.out.println(counts[i]);
+            System.out.print(candidates[i] + ": ");
+            System.out.println(counts[i] + " times picked by the system");
         }
     }
 }
